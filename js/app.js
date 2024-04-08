cb5c80332a274ab8b7510d274c9453da43d14f33
Lucas Mineiro <lucas0asm@outlook.com>
2024-04-08 01:05:43 -0300

    validação

--- js/app.js
+++ js/app.js
@@ -4,9 +4,19 @@ limpar ();
 function adicionar() {
     //recuperar valores: nome do produto, quantidade e valor
     let produto = document.getElementById('produto').value;
+    let quantidade = document.getElementById('quantidade').value;
+    //Verificar se o produto selecionado é válido
+    if (!produto || produto.trim() === "") {
+        alert("Selecione um produto válido.");
+        return;
+    }
+    //Verificar se a quantidade inserida é valida
+    if (isNaN(quantidade) || quantidade <=0) {
+        alert("Insira uma quantidade válida.");
+        return;
+    }
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade').value;
     //calcular o preço, o nosso subtotal
     let preco = quantidade * valorUnitario;
     //adicionar no carrinho
